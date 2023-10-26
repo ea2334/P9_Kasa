848294880f01352edad918d8ffe713b4c619848b
John Doe <johndoe@example.com>
2023-10-26 09:50:32 +0200

    .

--- Work/kasa/src/index.js
+++ Work/kasa/src/index.js
@@ -3,11 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home/Home';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
   </React.StrictMode>
 );
 
---
+++ Work/kasa/src/pages/Home/Home.jsx
@@ -0,0 +1,14 @@
+import Header from "../../components/Header/Header"
+
+function Home(){
+    return(
+        <>
+        <Header />
+        <h1>Accueil</h1>
+        </>
+        
+    )
+}
+
+
+export default Home
