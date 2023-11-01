40cc6900e7a17eff332421dbe3b6d0bf8d6953ef
John Doe <johndoe@example.com>
2023-11-01 16:34:09 +0100

    .

--- Work/kasa/src/components/Gallery/Card.jsx
+++ Work/kasa/src/components/Gallery/Card.jsx
@@ -3,7 +3,6 @@ function Card({ apartment }) {
     <div className="card">
       <img src={apartment.image} alt={apartment.title} className="card-image" />
       <h3 className="card-title">{apartment.title}</h3>
-      <p className="card-description">{apartment.description}</p>
     </div>
   );
 }
