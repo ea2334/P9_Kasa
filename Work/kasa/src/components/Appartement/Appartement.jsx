8bffa03840f5ba9b28eb8a33ba941cd27c418f15
John Doe <johndoe@example.com>
2023-11-09 09:49:26 +0100

    .

--- Work/kasa/src/components/Appartement/Appartement.jsx
+++ Work/kasa/src/components/Appartement/Appartement.jsx
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom";
 import React from "react";
+import Logements from "../../pages/Logements/Logements";
 
 function Appartement(props) {
 
@@ -8,8 +9,11 @@ console.log("", props);
     return (
         <div className="card">
            <img src={props.image} alt="Image de l'appartement" />
-           <div className="title_apart">{props.title}</div>
+           <Link to={`/logement/${props.id}`}>
+           <h3 className="title_apart">{props.title}</h3>
+           </Link>
         </div>
+        
     )
   }
   
--- Work/kasa/src/components/Gallery/Gallery.jsx
+++ Work/kasa/src/components/Gallery/Gallery.jsx
@@ -1,26 +1,19 @@
 import { useEffect, useState } from "react";
 import Appartement from "../Appartement/Appartement";
+import Datas from "../../data/data.json";
 
 
 
 function Gallery() {
 
-const {apartments, setApartment} = useState({});
 
-useEffect(fetchApartment, {});
 
-function fetchApartment(){
-fetch("./data.json")
-.then((res) => res.json())
-.then((res) => setApartment(res))
-.catch(console.error);
-}
 
     return (
       
       <div className="gallery">
-        {apartments.map((apartment) => (
-          <Appartement title={apartment.title} image={apartment.cover} />
+        {Datas.map((apartment) => (
+          <Appartement title={apartment.title} image={apartment.cover} key={apartment.id} />
         ))}
 
       </div>
